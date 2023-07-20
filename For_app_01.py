8e716230d0604fe5ce75eb134213e08c6ee0ba67
agustinval-utn <138217373+agustinval-utn@users.noreply.github.com>
2023-07-20 11:52:35 -0300

    primeros ejercicios de for

---
+++ For_app_01.py
@@ -0,0 +1,35 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+import random
+
+
+'''
+Al presionar el botón Mostrar 5 veces un mensaje (utilizando el Dialog Alert) con números ASCENDENTES, desde el 1 al 5.
+'''
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("UTN Fra")
+
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+
+
+    def btn_mostrar_on_click(self):
+        lista_numeros = list(range(0,11,2))
+        for numero in range(5):
+            print (numero)
+       
+            
+
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
